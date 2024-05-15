59146400de1f39f4c4a282f950b0f73a63635b71
Milan <milangru@gmail.com>
2024-05-15 23:49:08 +0200

    Added unit tests file.

---
+++ tests.py
@@ -0,0 +1,13 @@
+import main
+import unittest
+
+
+class TestMainFunctions(unittest.TestCase):
+    def test_triangle_type(self):
+        self.assertEqual(main.triangle_type(3, 4, 5), "Scalene and right.")
+
+    def test_is_triangle(self):
+        self.assertEqual(main.is_right_triangle(3, 4, 5), True)
+
+if __name__ == "__main__":
+    unittest.main()
