0bdc89a3ae64dfd6b89525f3da56a18d70ee633e
Milan <milangru@gmail.com>
2024-05-15 23:48:47 +0200

    Added direct execution to main.py.

--- main.py
+++ main.py
@@ -18,18 +18,20 @@ def is_right_triangle(a, b, c):
     '''Checks whether the given sides form a right-angled triangle.'''
     return (b**2 + a**2 == c**2)
 
-try:
-    a = round(float(input("Enter the length of side a: ")), 2)
-    b = round(float(input("Enter the length of side b: ")), 2)
-    c = round(float(input("Enter the length of side c: ")), 2)
+if __name__ == "__main__":
+    try:
+        a = round(float(input("Enter the length of side a: ")), 2)
+        b = round(float(input("Enter the length of side b: ")), 2)
+        c = round(float(input("Enter the length of side c: ")), 2)
 
-    if a <= 0 or b <= 0 or c <= 0:
-        print("ERROR \nAt least one side is negative or zero.") 
-    else:
-        triangle = triangle_type(a, b, c)
+        if a <= 0 or b <= 0 or c <= 0:
+            print("ERROR \nAt least one side is negative or zero.") 
+        else:
+            triangle = triangle_type(a, b, c)
+            print('-=' * 20)
+            print(f"The triangle is: {triangle}")
+
+    except ValueError:
         print('-=' * 20)
-        print(f"The triangle is: {triangle}")
-
-except ValueError:
-    print('-=' * 20)
+        print("ERROR \nPlease enter valid positive numbers.")
     print("ERROR \nPlease enter valid positive numbers.")
