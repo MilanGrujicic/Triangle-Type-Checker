bf9a7ce99770ae847df5fb1b6bb3e462d063065a
Milan <milangru@gmail.com>
2024-05-17 08:55:59 +0200

    Updated a test name.

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class TestMainFunctions(unittest.TestCase):
     def test_triangle_type(self):
         self.assertEqual(main.triangle_type(3, 4, 5), "Scalene and right.")
 
-    def test_is_triangle(self):
+    def test_is_right_triangle(self):
         self.assertEqual(main.is_right_triangle(3, 4, 5), True)
 
 if __name__ == "__main__":
